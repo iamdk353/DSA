6e2820713cf0ad8900a851b6bae55b1b95d03d0a
Dhanush Kumar M <142735077+iamdk353@users.noreply.github.com>
2026-01-27 07:01:37 +0530

    Time: 20 ms (6.59%), Space: 19.5 MB (47.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,32 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome(self, s: str) -> bool: 
         l=0
         r=len(s)-1
 
-        while l<r:
-
-            if not s[l].isalnum():
+        while(l<r):
+            if self.isNonAlphaNumeric(s[l]):
                 l+=1
                 continue
-            
-            if not s[r].isalnum():
+            if self.isNonAlphaNumeric(s[r]):
                 r-=1
                 continue
-            
-            if s[l].lower() != s[r].lower():return False
-
+           
+            if (s[l].lower() != s[r].lower()):
+                return False
             l+=1
             r-=1
+            
         return True
+    def isNonAlphaNumeric(self,s:str)->bool:
+            return (not((ord(s)>=ord("a") and ord(s)<=ord("z")) 
+                    or( ord(s)>=ord("A") and ord(s)<=ord("Z")) 
+                    or ( ord(s)>=ord("0") and ord(s)<=ord("9"))))
+
+                
+            
+            
+            
+        
+        
+
+
